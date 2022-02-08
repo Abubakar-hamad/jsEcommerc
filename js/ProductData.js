bdfff73b9bfbf90a55afaf5d7dbea0333cd5c08c
abubakar-hamad <abubakar.hmdelneel@gmail.com>
2022-02-08 20:42:53 +0200

    udapte-imgUrl3

--- js/ProductData.js
+++ js/ProductData.js
@@ -1,11 +1,11 @@
 let ProductsData = [
-    {id:1 ,isMe:'No' , quantity:1 , title:"samsung s 20" , color:'selver' , memory:'256 G' , price:700 , imgUrl:"/image/product/s20.jpg"},
-    {id:2 ,isMe:'No' , quantity:1 , title:"nokia 5G" , color:'gold' , memory:'128 G' , price:920 , imgUrl:"/image/product/mokiax50.jpg" },
-    {id:3 ,isMe:'No' , quantity:1 , title:"iphone 12 pro" , color:'green' , memory:'512 G' , price: 900 , imgUrl:"/image/product/iphone12pro.jpg" },
-    {id:4 ,isMe:'No' , quantity:1 , title:"iphone 12" , color:'lightblue' , memory:'128 G' , price:880 , imgUrl:"/image/product/iphone12.jpeg" },
-    {id:5 ,isMe:'No' , quantity:1 , title:"vivo x50" , color:'blue' , memory:'128 G' , price:199 , imgUrl:"/image/product/vivo-X50-Pro-2.jpg" },
-    {id:6 ,isMe:'No' , quantity:1 , title:"huawei mate 30" , color:'green' , memory:'256 G' , price:560 , imgUrl:"/image/product/mate30.jpg" },
-    {id:7 ,isMe:'No' , quantity:1 , title:"samsung s 21" , color:'selver' , memory:'128 G' , price:740 , imgUrl:"/image/product/galaxy-s21.png" },
+    {id:1 ,isMe:'No' , quantity:1 , title:"samsung s 20" , color:'selver' , memory:'256 G' , price:700 , imgUrl:"./image/product/s20.jpg"},
+    {id:2 ,isMe:'No' , quantity:1 , title:"nokia 5G" , color:'gold' , memory:'128 G' , price:920 , imgUrl:"./image/product/mokiax50.jpg" },
+    {id:3 ,isMe:'No' , quantity:1 , title:"iphone 12 pro" , color:'green' , memory:'512 G' , price: 900 , imgUrl:"./image/product/iphone12pro.jpg" },
+    {id:4 ,isMe:'No' , quantity:1 , title:"iphone 12" , color:'lightblue' , memory:'128 G' , price:880 , imgUrl:"./image/product/iphone12.jpeg" },
+    {id:5 ,isMe:'No' , quantity:1 , title:"vivo x50" , color:'blue' , memory:'128 G' , price:199 , imgUrl:"./image/product/vivo-X50-Pro-2.jpg" },
+    {id:6 ,isMe:'No' , quantity:1 , title:"huawei mate 30" , color:'green' , memory:'256 G' , price:560 , imgUrl:"./image/product/mate30.jpg" },
+    {id:7 ,isMe:'No' , quantity:1 , title:"samsung s 21" , color:'selver' , memory:'128 G' , price:740 , imgUrl:"./image/product/galaxy-s21.png" },
     // {id: , title:"" , color:'' , memory:' G' , address:'' , imgUrl:"../image/product/.jpg" },
 
 ]
